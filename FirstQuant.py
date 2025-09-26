f221bfc60a8bbe19bd6ee7743974299530b4fa47
bighent3 <111480085+bighent3@users.noreply.github.com>
2025-09-25 23:47:43 -0700

    Add files via upload

--- FirstQuant.py
+++ FirstQuant.py
@@ -1,18 +1,22 @@
 import dash
+import yfinance as yf
+import pandas as pd
 from dash import html, dcc
+
+
 
 # Create a dash application
 app = dash.Dash(__name__)
 
 # define the layout of the dashboard
 app.layout = html.Div(children=[
-    html.H1('My Dashboard'),
+    html.H2('Apple Stock Price Day of Relationship between Interest Rate Changes'),
     dcc.Graph(
         id='My-Graph',
         figure={
             'data':[
-                {'x':[1,2,3,4], 'y':[4,1,2,8], 'type': 'bar', 'name':'Bar chart'},
-                {'x':[1,2,3,4], 'y':[2,4,5,8], 'type': 'line', 'name':'Line chart'}
+                {'x':['July 31, 2019','September 18, 2019', 'October 30, 2019', 'March 3, 2020', 'March 15, 2020'], 'y':[0.25, 0.25, 0.25, 0.5, 1], 'type': 'bar', 'name':'Bar chart'},
+                {'x':['July 31, 2019','September 18, 2019', 'October 30, 2019', 'March 3, 2020', 'March 15, 2020'], 'z':[2,4,5,8], 'type': 'line', 'name':'Line chart'}
             ],
             'layout': {
                 'title':'Dash Data Visualization'
